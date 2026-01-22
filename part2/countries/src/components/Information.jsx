32414da2d7a5efeac75fc7b92cb92724114ee531
Kyle Montiel-Berner <kyle_montielberner@outlook.com>
2026-01-22 12:31:09 -0500

    WIP: weather exercise

--- part2/countries/src/components/Information.jsx
+++ part2/countries/src/components/Information.jsx
@@ -1,5 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const weather_key = import.meta.env.VITE_WEATHER_API_KEY;
+
 const Information = ({ country }) => {
   console.log("info received country", country);
+  console.log("API Key:", weather_key);
+  const [capitalWeather, setCapitalWeather] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&appid=${weather_key}`,
+      )
+      .then((response) => setCapitalWeather(response.data));
+  }, [country]);
+
   return (
     <div>
       <h1>{country.name}</h1>
@@ -12,6 +28,14 @@ const Information = ({ country }) => {
         ))}
       </ul>
       <span className="flag">{country.flag}</span>
+      {capitalWeather && (
+        <div>
+          <h1>Weather in ${capitalWeather.capital}</h1>
+          <h3>Temperature ${capitalWeather.main.temp}</h3>
+          <span></span>
+          <h3>Wind ${capitalWeather.wind.speed}</h3>
+        </div>
+      )}
     </div>
   );
 };
--- part2/countries/src/services/listActions.jsx
+++ part2/countries/src/services/listActions.jsx
@@ -2,9 +2,12 @@ import axios from "axios";
 
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all";
 
+
 const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data);
 };
+
+
 
 export default getAll;
 
