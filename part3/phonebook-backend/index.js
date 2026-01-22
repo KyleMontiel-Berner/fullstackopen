b5717a056968eefe02e36462a9de09eac0c7fded
Kyle Montiel-Berner <kyle_montielberner@outlook.com>
2026-01-22 15:17:18 -0500

    "Clean up renamed folder and fix backend for Render deployment"

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 let notes = [
@@ -29,6 +30,7 @@ morgan.token("userContent", (request) => {
   return JSON.stringify(request.body);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(
@@ -89,7 +91,7 @@ app.post("/api/persons", (request, response) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
