eeec656ddda3bcfb540b4243ca2ec65ebb20f77b
Kyle Montiel-Berner <kyle_montielberner@outlook.com>
2026-02-05 20:43:44 -0500

    Make login permanent with logout functionality

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -14,11 +14,20 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedBlogUserJSON = window.localStorage.getItem("loggedBlogUser");
+    if (loggedBlogUserJSON) {
+      const user = JSON.parse(loggedBlogUserJSON);
+      setUser(user);
+    }
+  }, []);
+
   const handleLogin = async (event) => {
     event.preventDefault();
     console.log("attempted login", { username, password });
     try {
       const user = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedBlogUser", JSON.stringify(user));
       console.log("login successful:", user);
       setUser(user);
       setUsername("");
@@ -32,39 +41,54 @@ const App = () => {
     }
   };
 
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <label>
-            username
-            <input
-              type="text"
-              value={username}
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </label>
-          <label>
-            password
-            <input
-              type="text"
-              value={password}
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </label>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    );
-  }
-  return (
+  const loginForm = () => (
     <div>
-      <h2>blogs</h2>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <label>
+          username
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+        <label>
+          password
+          <input
+            type="text"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+
+  const loggedInBlogs = () => (
+    <div>
       <p>{user.name} logged in</p>
+      <button
+        type="submit"
+        onClick={() => {
+          setUser(null);
+          window.localStorage.removeItem("loggedBlogUser");
+        }}
+      >
+        logout
+      </button>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
+    </div>
+  );
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {!user && loginForm()}
+      {user && loggedInBlogs()}
     </div>
   );
 };
