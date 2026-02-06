a92e367a6a4042eb027045b616509809c9045e75
Kyle Montiel-Berner <kyle_montielberner@outlook.com>
2026-02-05 19:48:25 -0500

    Add login functionality

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,24 +1,71 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log("attempted login", { username, password });
+    try {
+      const user = await loginService.login({ username, password });
+      console.log("login successful:", user);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      console.log("login error:", error);
+      setErrorMsg("wrong credentials");
+      setTimeout(() => {
+        setErrorMsg(null);
+      }, 5000);
+    }
+  };
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+          <label>
+            password
+            <input
+              type="text"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    );
+  }
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <p>{user.name} logged in</p>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
