471c9896772ce250a48d4ceb195c49470437e0cb
Kyle Montiel-Berner <kyle_montielberner@outlook.com>
2026-05-28 15:28:02 -0400

    Add route handlers

--- part13/RDBS/index.js
+++ part13/RDBS/index.js
@@ -46,13 +46,37 @@ Blog.init(
   },
 );
 
-app.get("api/blogs", async (req, res) => {
+app.get("/api/blogs", async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
 });
 
-app.post("api/blogs", async (req, res) => {
-  const blog = await Blog.create(...req.body);
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = await Blog.create({ ...req.body });
+    res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    await blog.destroy();
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.get("/api/blogs:id", async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id);
+    res.json(blog);
+  } catch (error) {
+    res.status(404).end();
+  }
 });
 
 const PORT = process.env.PORT || 3001;
