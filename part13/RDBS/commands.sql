980d2e25892ec67a1a29c20d862bcf376c245411
Kyle Montiel-Berner <kyle_montielberner@outlook.com>
2026-05-28 14:04:12 -0400

    Add new Blog table and class with query capabilities

---
+++ part13/RDBS/commands.sql
@@ -0,0 +1,6 @@
+INSERT INTO blogs (author, url, title, likes)
+VALUES ('Kyle Montiel-Berner', 'www.rdbsmagic.com', 'RDBS Magic', 5);
+
+
+INSERT INTO blogs (author, url, title, likes)
+VALUES ('Player 2', 'www.playerrdbs.com', 'Playing RDBS Magic', 3);
--- part13/RDBS/index.js
+++ part13/RDBS/index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const express = require("express");
+const app = express();
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
@@ -11,17 +13,47 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    const notes = await sequelize.query("SELECT * from notes", {
-      type: QueryTypes.SELECT,
-    });
-    console.log(notes);
-    sequelize.close();
-  } catch (error) {
-    console.error("Unable to connect to database:", error);
-  }
-};
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscore: true,
+    timestamps: false,
+    modelName: "blog",
+  },
+);
 
-main();
+app.get("api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+app.post("api/blogs", async (req, res) => {
+  const blog = await Blog.create(...req.body);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
